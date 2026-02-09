e9265f93f96e4ea1c1e5f2326663cb0fc1c8bd3c
TheAxeWelder <10rgokart@gmail.com>
2026-02-08 19:02:47 -0500

    Controller support

--- Assets/CarScript.cs
+++ Assets/CarScript.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class CarScript : MonoBehaviour
 {
     public float acc = 5000f;
     public float speed;
+    public float motor;
     public float slowDown = 5000f;
     public float brakeForce = 10000f;
     public float turnSpeed = 50.0f;
@@ -18,40 +20,90 @@ public class CarScript : MonoBehaviour
     public WheelCollider rearleftWheel;
     public WheelCollider rearrightWheel;
 
+    public Text speedText;
+
+
+    public InputActionReference steering;
+    public InputActionReference throttle;
+    public InputActionReference brake;
+    public InputActionReference gearChange;
+
     private float horizontalInput;
     private float verticalInput;
+    private float brakeInput;
     private Rigidbody rb;
+    public bool controller;
+    public bool keyboard;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.drag = 0.1f;
         rb.angularDrag = 0.25f;
+        rb.centerOfMass = new Vector3(0, -0.5f, 0);
+
+        if(controller)
+        {
+            steering.action.Enable();
+            throttle.action.Enable();
+            brake.action.Enable();
+            gearChange.action.Enable();
+        }
+        if(keyboard)
+        {
+            steering.action.Disable();
+            throttle.action.Disable();
+            brake.action.Disable();
+            gearChange.action.Disable();
+        }
     }
+
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        if (controller)
+        {
+            horizontalInput = steering.action.ReadValue<float>();
+            float rawThrottle = throttle.action.ReadValue<float>();
+            verticalInput = Mathf.Clamp01((rawThrottle + 1f) * 0.5f);
+            float rawBrake = brake.action.ReadValue<float>();
+            brakeInput = Mathf.Clamp01((1f - rawBrake) * 0.5f);
+        }
+        else
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+        }
 
+
+        speedText.text = "Speed: " + verticalInput;
+        Debug.Log(brakeInput);
         speed = rb.velocity.magnitude * 2.237f;
     }
 
     void FixedUpdate()
     {
         float currentBrake = 0f;
+        motor = verticalInput * acc;
 
-        if (Input.GetKey(KeyCode.Space))
+        if (controller)
         {
-            currentBrake = brakeForce;
+            if (brakeInput > 0.05f)
+            {
+                currentBrake = brakeInput * brakeForce;
+                motor = 0f;
+            }
+
         }
-        else if (Mathf.Abs(verticalInput) < 0.1f)
+
+        if (keyboard)
         {
-            currentBrake = slowDown;
+            if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Z))
+            {
+                currentBrake = brakeForce;
+                motor = 0f;
+            }
         }
-
-
-        float motor = verticalInput * acc;
 
         frontleftWheel.motorTorque = motor;
         frontrightWheel.motorTorque = motor;
